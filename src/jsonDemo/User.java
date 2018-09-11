15025061ae0d87314536fe56485aa061c0f45d56
brianxautumn <shred@brianautumn.com>
2018-09-10 20:10:11 -0700

    Adding workspace and updates

--- src/jsonDemo/User.java
+++ src/jsonDemo/User.java
@@ -5,9 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class User {
-
+  // Use Java generics to avoid linear searching for users by id, makes it O(1) instead of O(n)
   private  static Map<Integer, User> useridDict = new HashMap<>();
-  private  static Map<String, User> usernameDict = new HashMap<>();
   private static ArrayList<User> allUsers = new ArrayList<>();
 
   public void setUsername(String username) {
@@ -31,6 +30,7 @@ public class User {
   public User(String username, int userid){
     this.username = username;
     this.userid = userid;
+    allUsers.add(this);
     useridDict.put(userid, this);
   }
 
